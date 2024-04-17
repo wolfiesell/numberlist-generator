a019852e8584d7ca4fd6c35aead27c5f45a42160
hairychessat15 <124609795+hairychessat15@users.noreply.github.com>
2024-04-16 23:28:07 -0500

    Add files via upload

---
+++ wordlist_generator.py
@@ -0,0 +1,11 @@
+for a in range(10):
+	for b in range(10):
+		for c in range(10):
+			for d in range(10):
+				for e in range(10):
+					for f in range(10):
+						for g in range(10):
+							for h in range(10):
+								for i in range(10):
+									print(f"{a}{b}{c}{d}{e}{f}{g}{h}{i}")
+									
