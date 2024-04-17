b0e18b9094c43c69640ef5b1d1ecd833871f4d4f
hairychessat15 <124609795+hairychessat15@users.noreply.github.com>
2024-04-16 23:31:05 -0500

    Update and rename wordlist_generator.py to numberlist_generator.py

--- numberlist_generator.py
+++ numberlist_generator.py
@@ -1,3 +1,4 @@
+#Probably inefficient, but will produce a text file of '000000000' to '999999999' one number per line for brute force
 for a in range(10):
 	for b in range(10):
 		for c in range(10):
@@ -8,4 +9,5 @@ for a in range(10):
 							for h in range(10):
 								for i in range(10):
 									print(f"{a}{b}{c}{d}{e}{f}{g}{h}{i}")
+									
 									
